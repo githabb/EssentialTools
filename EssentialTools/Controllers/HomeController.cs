77febb20e6886364b80c9947c91f92ca29f20469
yurievch <yurievch@gmail.com>
2016-06-16 19:23:17 +0300

    первый коммит

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IValueCalculator calc;
+
+        private Product[] products = {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 275M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 48.95M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 19.50M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 34.95M}
+        };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
